fb42f5250f3a83f16c7799e6722421e44287a0b5
geekgit <geeekz@gmail.com>
2014-02-14 17:10:25 +0600

    Initial lib + Console tool

---
+++ NetworkLib/NetworkLib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class NetworkLib
+{
+    public static int Hello()
+    {
+        return 4;
+    }
+}
+
